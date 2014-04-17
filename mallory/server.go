5a62d06c306ecb8873fe70f6f79b70413ca12c1e
jianjun <jianjun@atrenta.com>
2014-04-17 13:39:47 +0800

    clean code

--- mallory/server.go
+++ mallory/server.go
@@ -79,30 +79,29 @@ func (self *Server) NewID() int64 {
 //
 func (self *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s := CreateSession(self, w, r)
+	atomic.AddInt64(&self.CountAlive, 1)
 
 	s.Info("%s %s %s", r.Method, r.URL.Host, r.Proto)
-
-	// This is an error if is not empty on Client
-	r.RequestURI = ""
-	// If no Accept-Encoding header exists, Transport will add the headers it can accept
-	// and would wrap the response body with the relevant reader.
-	r.Header.Del("Accept-Encoding")
-	// curl can add that, see
-	// http://homepage.ntlworld.com/jonathan.deboynepollard/FGA/web-proxy-connection-header.html
-	r.Header.Del("Proxy-Connection")
-	// Connection is single hop Header:
-	// http://www.w3.org/Protocols/rfc2616/rfc2616.txt
-	// 14.10 Connection
-	//   The Connection general-header field allows the sender to specify
-	//   options that are desired for that particular connection and MUST NOT
-	//   be communicated by proxies over further connections.
-	r.Header.Del("Connection")
-
-	atomic.AddInt64(&self.CountAlive, 1)
 
 	if r.Method == "CONNECT" {
 		self.Engine.Connect(s)
 	} else {
+		// This is an error if is not empty on Client
+		r.RequestURI = ""
+		// If no Accept-Encoding header exists, Transport will add the headers it can accept
+		// and would wrap the response body with the relevant reader.
+		r.Header.Del("Accept-Encoding")
+		// curl can add that, see
+		// http://homepage.ntlworld.com/jonathan.deboynepollard/FGA/web-proxy-connection-header.html
+		r.Header.Del("Proxy-Connection")
+		// Connection is single hop Header:
+		// http://www.w3.org/Protocols/rfc2616/rfc2616.txt
+		// 14.10 Connection
+		//   The Connection general-header field allows the sender to specify
+		//   options that are desired for that particular connection and MUST NOT
+		//   be communicated by proxies over further connections.
+		r.Header.Del("Connection")
+
 		self.Engine.Serve(s)
 	}
 
